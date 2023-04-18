b024061777d93f774069d75e285bf376182b0cde
Gene Suhir <gsuhir@gmail.com>
2023-04-18 08:17:39 -0700

    set consts and vars, but still not generating a password

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
+  const lowercase = "abcdefghijklmnopqrstuvwxyz"
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const numbers = "0123456789"
+  const special = "!@#$%^&*()"
 
-  passwordText.value = password;
+  let passwordLength;
+  let includeLowercase;
+  let includeUppercase;
+  let includeNumbers;
+  let includeSpecial; 
 
+  do {
+    passwordLength = prompt("Please select a password length between 8 and 128 characters");
+  } while (passwordLength < 8 || passwordLength > 128);
+  
+  do {
+    includeLowercase = confirm("Would you like to include lowercase letters?");
+    includeUppercase = confirm("Would you like to include UPPERCASE letters?");
+    includeNumbers = confirm("Would you like to include digits (0-9)?");
+    includeSpecial = confirm("Would you like to include any of the following special characters -- \u0022!@#$%^&*()\u0022?");
+  } while (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial);
+
+  let possibleCharacters = '';
+  if (includeLowercase) {
+    possibleCharacters += lowercase;
+  } if (includeUppercase) {
+    possibleCharacters += uppercase;
+  } if (includeNumbers) {
+      possibleCharacters += numbers;
+  } if (includeSpecial) {
+    possibleCharacters += special;
+  }
+
+  var password = "";
+  for (let i=0; i < passwordLength; i++) {
+    password += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+  } return password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
